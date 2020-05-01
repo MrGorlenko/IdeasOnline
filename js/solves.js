315a95c40bce24c16ebf0dbcbb3604e2dc81a0eb
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-05-01 18:02:02 +0300

    running line

---
+++ js/solves.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+
+    // $(function() {
+    //     $('.background-label').marquee({
+    //       duration: 5000,
+    //       startVisible: true,
+    //       duplicated: true
+    //     });
+    //   });
+
+    // $(function() {
+    //     $('.background-star').marquee({
+    //       duration: 7000,
+    //       startVisible: true,
+    //       duplicated: true
+    //     });
+    //   });
+
+    //   background-star
+
+})
