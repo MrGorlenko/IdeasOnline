b2a6e7251f6fd8570ee65b9564e000627d193068
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-05-01 18:32:31 +0300

    running line new

--- js/solves.js
+++ js/solves.js
@@ -9,6 +9,25 @@ $(document).ready(function(){
     //     });
     //   });
 
+    $(function() {
+        $('.marquee').marquee({
+          duration: 7000,
+          startVisible: true,
+          duplicated: true
+        });
+      });
+
+      $(function() {
+        $('.marquee_r').marquee({
+          duration: 7000,
+          startVisible: true,
+          duplicated: true,
+          direction: 'right'
+        });
+      });
+
+    //   direction:
+
     // $(function() {
     //     $('.background-star').marquee({
     //       duration: 7000,
