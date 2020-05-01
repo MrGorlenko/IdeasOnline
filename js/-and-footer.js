75c507432cb507637cb68395a21cf9da8c2e2069
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-05-01 15:34:31 +0300

    fixing animations and logo

--- js/-and-footer.js
+++ js/-and-footer.js
@@ -13,4 +13,17 @@ $(document).ready(function(){
   }
 });
 
+
+
+    var target1 = $('.footer__rocket');
+    var targetPos1 = target1.offset().top;
+    var winHeight1 = $(window).height();
+    var scrollToElem1 = targetPos1 - winHeight1;
+    $(window).scroll(function(){
+    var winScrollTop1= $(this).scrollTop();
+    if(winScrollTop1 > scrollToElem1){
+        target1.addClass('rocket__animation_footer');
+    }
+    });
+
 })
